1e4e99ab348fe441925dee79d9b1c0f8acf4e861
Luigi Gentile <gntlg00@gmail.com>
2023-05-11 12:41:33 +0200

    up

--- server/routes/sensors.js
+++ server/routes/sensors.js
@@ -3,6 +3,7 @@ const Sensor = require('../models/Sensor');
 
 const router = express.Router();
 
+// get data
 router.get('/data', async (req, res) => {
     try {
         const sensors = await Sensor.find().sort({ timestamp: -1 }).select('-_id');
@@ -12,5 +13,19 @@ router.get('/data', async (req, res) => {
         res.status(500).send("Internal server error");
     }
 })
+
+// dati degli ultimi 30 giorni
+router.get('/last', async (req, res) => {
+    try {
+        const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+        const sensors = await Sensor.find({ timestamp: { $gt: thirtyDaysAgo } })
+            .sort({ timestamp: -1 })
+            .select('-_id');
+        res.send(sensors);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+    }
+});
 
 module.exports = router;
